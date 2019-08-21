0c515faa0e0abaae363aa1175be6d07400eee03f
AK <ak@AKs-MacBook-Pro.local>
2019-08-21 12:19:47 -0700

    [feat] working on creating seeder function to seed mongo database

---
+++ database/seeder.js
@@ -0,0 +1,32 @@
+//product category ids
+//1 - 25 Dresses
+//26-50 Pants
+//51-75 Skirts
+//76-100 Bedding
+
+//product categories
+const productCategories = [
+  'Dress', 'Pants', 'Skirts', 'Bedding'
+]
+//product subcategories
+const dressesSubcategory = [
+  'Maxi Dress', 'Midi Dress', 'Mini Dress', 'Jumpsuit',
+  'Sheath Dress', 'Pleated Dress'
+]
+const dressesAdjectivies = [
+  'Floral', 'Embroidered', 'Watercolor', 'Knit'
+]
+const productMainName = [
+  'Troubadour', 'Osprey', 'Luciana', 'Madeline'
+]
+const pantsSubcategory = [
+  'Pants', 'Trousers', 'Joggers', 'Jeans', 
+  'Jumpsuit', 'Overalls', 'Leggings'
+]
+const skirtsSubcategory = [
+  'Midi Skirt', 'Maxi Skirt', 'Pencil Skirt', 'Mini Skirt', 
+  'Wrap Skirt'
+]
+const BeddingSubcategory = [
+  'Quilt', 'Cover', 'Blanket'
+]
