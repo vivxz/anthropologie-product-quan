a4d821012d3236026327b2f89f1e87007ca46dfb
AK <ak@AKs-MacBook-Pro.local>
2019-09-04 08:12:47 -0700

    [refactor]Clean up stylesheets and jsx files

--- client/src/components/MainPictureDisplay.jsx
+++ client/src/components/MainPictureDisplay.jsx
@@ -3,34 +3,19 @@ import MainPictureSlide from './MainPictureSlide.jsx';
 
 let MainPictureDisplay = ({ intialRenderOfMainPic, mainPictureIndex, pictureArray, transformPictureValue }) => {
   if (pictureArray.length > 0) {
-    // if (intialRenderOfMainPic) {
     return (
-      // <div className='main-picture'>
         <div className='main-picture-container'
           style={
             {
-              transform: `translateX(${transformPictureValue})`,
+              transform: `translateX(${transformPictureValue}px)`,
               transition: 'transform ease-out 0.5s'
             }
           }>
           {pictureArray.map((image, i) => (
             <MainPictureSlide image={image} key={i} />
           ))}
-          {/* <img src={`${pictureArray[mainPictureIndex]}`}></img> */}
         </div>
-      // </div>
     )
-    // } 
-    // else {
-    //   return (
-    //     <div className='main-picture-container'>
-    //       <img src={`${pictureArray[mainPictureIndex]}`} className='slide-main-img'></img>
-    //       <div className='caption'>
-    //         Hover your mouse over an image to zoom.
-    //       </div>
-    //     </div>
-    //   )
-    // }
   } else {
     return (<div></div>)
   }
